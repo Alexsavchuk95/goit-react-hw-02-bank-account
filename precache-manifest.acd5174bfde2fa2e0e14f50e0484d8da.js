2c6c2613a8e3d0f30e74701623291893fa0be1df
Alexsavchuk95 <savchukalex57@gmail.com>
2019-11-24 01:16:47 +0300

    Updates

---
+++ precache-manifest.acd5174bfde2fa2e0e14f50e0484d8da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd8eb4c875fcd547ec2a5080dc02bca",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "46b2fececb50503e408f",
+    "url": "/goit-react-hw-02-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "e4566c028fe3b92efa98",
+    "url": "/goit-react-hw-02-bank-account/static/css/main.df35bf83.chunk.css"
+  },
+  {
+    "revision": "46b2fececb50503e408f",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.a9cab04c.chunk.js"
+  },
+  {
+    "revision": "e4566c028fe3b92efa98",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.c3c3bf7e.chunk.js"
+  },
+  {
+    "revision": "1f5ad8073d8eccbd6674",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.50193cb7.js"
+  }
+]);
